03ecda69799bd7c78818fa525d396006521e5021
Allen Luce <allen@github.con.com>
2019-08-14 11:17:58 -0700

    Remove semicolons, var statements
    Semicolons aren't necessary. Good to have vars labeled with const or let.

--- cli.js
+++ cli.js
@@ -1,31 +1,30 @@
 #!/usr/bin/env node
+const _ = require('lodash')
+const AWS = require('aws-sdk')
+const chalk = require('chalk')
+const moment = require('moment')
+const commander = require('commander')
 
-var _ = require('lodash');
-var AWS = require('aws-sdk');
-var chalk = require('chalk');
-var moment = require('moment');
-var commander = require('commander');
-
-var lowPrice = false;
-var lowZone = false;
+let lowPrice = false
+let lowZone = false
 
 exports.getRegions = function (instanceTypes) {
   return new Promise(function(resolve, reject) {
-    var ec2 = new AWS.EC2({apiVersion: '2016-04-01', region: 'us-east-2'});
+    const ec2 = new AWS.EC2({apiVersion: '2016-04-01', region: 'us-east-2'})
     ec2.describeRegions({}, function(err, data) {
       if (err) {
-        reject(err);
-        return;
+        reject(err)
+        return
       }
       if (data && data.Regions) {
-        var promises = _.map(data.Regions, region => exports.getRegionSpots(region, instanceTypes))
-        Promise.all(promises).then(resolve).catch(reject);
+        const promises = _.map(data.Regions, region => exports.getRegionSpots(region, instanceTypes))
+        Promise.all(promises).then(resolve).catch(reject)
       } else {
-        reject(new Error("Did not get region data from AWS"));
-        return;
+        reject(new Error("Did not get region data from AWS"))
+        return
       }
-    });
-  }).then(_.flatten);
+    })
+  }).then(_.flatten)
 }
 
 function uniqByZoneAndType(instances) {
@@ -34,12 +33,12 @@ function uniqByZoneAndType(instances) {
 
 exports.getRegionSpots = function (region, instanceTypes) {
   return new Promise(function(resolve, reject) {
-    var ec2 = new AWS.EC2({apiVersion: '2016-04-01', region: region.RegionName});
-    var params = {
+    const ec2 = new AWS.EC2({apiVersion: '2016-04-01', region: region.RegionName})
+    const params = {
       InstanceTypes: instanceTypes,
       ProductDescriptions: ['Linux/UNIX', 'Linux/UNIX (Amazon VPC)'],
       StartTime: moment().subtract(4, 'hours').utc().toDate()
-    };
+    }
     ec2.describeSpotPriceHistory(params, function(err, data) {
       if (err) {
         if (err.code && err.code === 'AuthFailure') {
@@ -47,65 +46,65 @@ exports.getRegionSpots = function (region, instanceTypes) {
           resolve()
           return
         } else {
-          reject(err);
-          return;
+          reject(err)
+          return
         }
       }
       if (data && data.SpotPriceHistory) {
-        var instances = _.map(data.SpotPriceHistory, sdata => {
+        const instances = _.map(data.SpotPriceHistory, sdata => {
           if (sdata.SpotPrice && sdata.Timestamp && sdata.AvailabilityZone) {
-            var sdate = moment(sdata.Timestamp);
+            const sdate = moment(sdata.Timestamp)
             if (!lowPrice || Number(sdata.SpotPrice) < lowPrice) {
-              lowPrice = Number(sdata.SpotPrice);
-              lowZone = sdata.AvailabilityZone;
+              lowPrice = Number(sdata.SpotPrice)
+              lowZone = sdata.AvailabilityZone
             }
             return {
               lastDate: sdate,
               lastPrice: sdata.SpotPrice,
               zone: sdata.AvailabilityZone,
               itype: sdata.InstanceType
-            };
+            }
           }
-        });
-        resolve(instances);
+        })
+        resolve(instances)
       }
-    });
-  }).then(uniqByZoneAndType);
+    })
+  }).then(uniqByZoneAndType)
 }
 
 function handleResults (results, n, instanceTypes) {
-  console.log('\n' + _.padEnd('Instance Type', 16) + ' ' + _.padEnd('AWS Zone', 24) + ' ' + _.padEnd('Hourly Rate', 12));
-  console.log(_.pad('', 16, '-') + ' ' + _.pad('', 24, '-') + ' ' + _.pad('', 12, '-'));
-  sortedInstances = _.sortBy(results, val => Number(val.lastPrice), 'zone');
+  console.log('\n' + _.padEnd('Instance Type', 16) + ' ' + _.padEnd('AWS Zone', 24) + ' ' + _.padEnd('Hourly Rate', 12))
+  console.log(_.pad('', 16, '-') + ' ' + _.pad('', 24, '-') + ' ' + _.pad('', 12, '-'))
+  sortedInstances = _.sortBy(results, val => Number(val.lastPrice), 'zone')
   if (n > 0) {
     sortedInstances = sortedInstances.slice(0, n)
   }
   _.each(sortedInstances, function(data) {
-    var msg = _.padEnd(data.itype, 16) + ' ' + _.padEnd(data.zone, 24) + ' ' + _.padEnd('$' + data.lastPrice, 12);
+    let msg = _.padEnd(data.itype, 16) + ' ' + _.padEnd(data.zone, 24) + ' ' + _.padEnd('$' + data.lastPrice, 12)
     if (data.zone === lowZone && Number(data.lastPrice) === Number(lowPrice)) {
-      msg = chalk.green(msg);
+      msg = chalk.green(msg)
     } else if (lowPrice * 1.1 >= data.lastPrice) {
-      msg = chalk.yellow(msg);
+      msg = chalk.yellow(msg)
     }
-    console.log(msg);
-  });
+    console.log(msg)
+  })
   if (lowPrice) {
-    console.log('\n' + chalk.green('Cheapest hourly rate for [' + instanceTypes.join(', ') + '] is $' + lowPrice + ' in zone ' + lowZone));
+    console.log('\n' + chalk.green('Cheapest hourly rate for [' + instanceTypes.join(', ') + '] is $' + lowPrice + ' in zone ' + lowZone))
   } else {
-    console.log(chalk.yellow('No data found, did you specify a valid instance type?'));
+    console.log(chalk.yellow('No data found, did you specify a valid instance type?'))
   }
 }
 
 exports.standalone = function () {
-  lowPrice = false;
-  lowZone = false;
-  const program = new commander.Command();
+  lowPrice = false
+  lowZone = false
+  const program = new commander.Command()
   program
     .version('0.4.0')
     .usage('[options] <EC2 instance types ...>')
     .option('-r, --region <AWS region>', 'Limit to the given region')
     .option('-n, --number <number to show>', 'Only show the top few cheapest spots')
-    .parse(process.argv);
+    .parse(process.argv)
 
   if (program.args.length == 0) {
     program.outputHelp()
@@ -113,10 +112,10 @@ exports.standalone = function () {
   }
 
   const instanceTypes = program.args
-  console.log('Checking spot prices for [' + instanceTypes.join(', ') + '] instance type(s).');
+  console.log('Checking spot prices for [' + instanceTypes.join(', ') + '] instance type(s).')
 
   if (program.region) {
-    console.log('Limiting results to region ' + program.region);
+    console.log('Limiting results to region ' + program.region)
     return exports.getRegionSpots({RegionName: program.region}, instanceTypes).then(_.curryRight(handleResults)(program.number, instanceTypes))
   } else {
     return exports.getRegions(instanceTypes).then(_.curryRight(handleResults)(program.number, instanceTypes))
